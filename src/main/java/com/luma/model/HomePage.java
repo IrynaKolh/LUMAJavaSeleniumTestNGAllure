1169ea5cd62010cfa45e93a8913291b2f0053782
Iryna Kolhanova <iryna.kolhanova@gmail.com>
2024-06-17 13:11:44 -0400

    fix tab size

--- src/main/java/com/luma/model/HomePage.java
+++ src/main/java/com/luma/model/HomePage.java
@@ -2,8 +2,8 @@ package com.luma.model;
 
 import org.openqa.selenium.WebDriver;
 
-public class HomePage extends NavMenu{
-  public HomePage(WebDriver driver) {
-    super(driver);
-  }
+public class HomePage extends NavMenu {
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
 }
--- src/test/java/com/luma/test/ProductPageTest.java
+++ src/test/java/com/luma/test/ProductPageTest.java
@@ -14,34 +14,34 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ProductPageTest extends BaseTest {
-  @Test(
-    description = "TC-02 Testing 1 Product",
-    groups = {"Smoke", "Regretion"},
-    testName = "PRODUCT | Testing 1 Product"
-  )
-  @Story("Products")
-  @Severity(SeverityLevel.BLOCKER)
-  @Description("TC-02 Testing 1 Product")
-  @Link(TestData.BASE_URL)
-  public void testProduct() {
-    Allure.step("Go to base url");
-    getDriver().get(TestData.BASE_URL);
+    @Test(
+          description = "TC-02 Testing 1 Product",
+          groups = {"Smoke", "Regretion"},
+          testName = "PRODUCT | Testing 1 Product"
+    )
+    @Story("Products")
+    @Severity(SeverityLevel.BLOCKER)
+    @Description("TC-02 Testing 1 Product")
+    @Link(TestData.BASE_URL)
+    public void testProduct() {
+        Allure.step("Go to base url");
+        getDriver().get(TestData.BASE_URL);
 
-    ProductPage productPage = new HomePage(getDriver())
-      .clickGearTopMenu()
-      .clickBagsSideMenu()
-      .clickProductImg(TestData.DRIVEN_BACKPACK_PRODUCT_NAME);
+        ProductPage productPage = new HomePage(getDriver())
+              .clickGearTopMenu()
+              .clickBagsSideMenu()
+              .clickProductImg(TestData.DRIVEN_BACKPACK_PRODUCT_NAME);
 
-    final String actualProductName = productPage.getProductName();
-    final String breadcrumbsMenuText = productPage.getBreadcrumbsMenuText();
+        final String actualProductName = productPage.getProductName();
+        final String breadcrumbsMenuText = productPage.getBreadcrumbsMenuText();
 
-    Allure.step(
-      "Verify actual '" + actualProductName + "' equals to '" + TestData.DRIVEN_BACKPACK_PRODUCT_NAME + "'"
-    );
-    Assert.assertEquals(actualProductName, TestData.DRIVEN_BACKPACK_PRODUCT_NAME);
-    Allure.step(
-      "Verify actual '" + breadcrumbsMenuText + "' equals to '" + TestData.DRIVEN_BACKPACK_PRODUCT_PAGE_BREADCRUMBS_MENU + "'"
-    );
-    Assert.assertEquals(breadcrumbsMenuText, TestData.DRIVEN_BACKPACK_PRODUCT_PAGE_BREADCRUMBS_MENU);
-  }
+        Allure.step(
+              "Verify actual '" + actualProductName + "' equals to '" + TestData.DRIVEN_BACKPACK_PRODUCT_NAME + "'"
+        );
+        Assert.assertEquals(actualProductName, TestData.DRIVEN_BACKPACK_PRODUCT_NAME);
+        Allure.step(
+              "Verify actual '" + breadcrumbsMenuText + "' equals to '" + TestData.DRIVEN_BACKPACK_PRODUCT_PAGE_BREADCRUMBS_MENU + "'"
+        );
+        Assert.assertEquals(breadcrumbsMenuText, TestData.DRIVEN_BACKPACK_PRODUCT_PAGE_BREADCRUMBS_MENU);
+    }
 }
